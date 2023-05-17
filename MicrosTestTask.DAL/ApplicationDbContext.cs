2104d9bb4ab02c5b3c69e6c4158070a6c014812b
7kruger <ekruug3r@gmail.com>
2023-05-17 13:50:14 +0500

    add configurations

--- MicrosTestTask.DAL/ApplicationDbContext.cs
+++ MicrosTestTask.DAL/ApplicationDbContext.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MicrosTestTask.DAL.Configurations;
 using MicrosTestTask.DAL.Entities;
-using MicrosTestTask.Domain.Enums;
 
 namespace MicrosTestTask.DAL;
 
@@ -16,84 +16,9 @@ public class ApplicationDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<User>(builder =>
-        {
-            builder.ToTable("Users").HasKey(x => x.Id);
-
-            builder.HasData(new User
-            {
-                Id = 1,
-                Name = "admin",
-                Password = "a03ab19b866fc585b5cb1812a2f63ca861e7e7643ee5d43fd7106b623725fd67", // захешированный пароль '123'
-                Role = Role.Admin,
-                RegistrationDate = DateTime.Now,
-                IsBlocked = false
-            });
-            builder.HasData(new User
-            {
-                Id = 2,
-                Name = "kruger",
-                Password = "a03ab19b866fc585b5cb1812a2f63ca861e7e7643ee5d43fd7106b623725fd67", // захешированный пароль '123'
-                Role = Role.User,
-                RegistrationDate = DateTime.Now,
-                IsBlocked = false
-            });
-
-            builder.Property(x => x.Id).ValueGeneratedOnAdd();
-
-            builder.Property(x => x.Password).IsRequired();
-            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
-
-            builder.HasOne(x => x.Profile)
-                .WithOne(x => x.User)
-                .HasPrincipalKey<User>(x => x.Id)
-                .OnDelete(DeleteBehavior.Cascade);
-        });
-
-        modelBuilder.Entity<Profile>(builder =>
-        {
-            builder.ToTable("Profiles").HasKey(x => x.Id);
-
-            builder.Property(x => x.Id).ValueGeneratedOnAdd();
-            builder.Property(x => x.ImgRef).IsRequired(false);
-
-            builder.HasData(new Profile
-            {
-                Id = 1,
-                ImgRef = "/images/person.svg",
-                UserId = 1
-            });
-            builder.HasData(new Profile
-            {
-                Id = 2,
-                ImgRef = "/images/person.svg",
-                UserId = 2
-            });
-        });
-
-        modelBuilder.Entity<Category>(builder =>
-        {
-            builder.HasData(new Category
-            {
-                Id = 1,
-                Name = "Иные доходы",
-                CategoryType = CategoryType.Income
-            });
-            builder.HasData(new Category
-            {
-                Id = 2,
-                Name = "Другое",
-                CategoryType = CategoryType.Expense
-            });
-
-            builder.HasMany(x => x.Operations)
-                .WithOne(x => x.Category);
-        });
-
-        modelBuilder.Entity<Operation>(builder =>
-        {
-            builder.HasOne(x => x.Category)
-                .WithMany(x => x.Operations);
-        });
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new ProfileConfiguration());
+        modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+        modelBuilder.ApplyConfiguration(new OperationConfiguration());
     }
 }
---
+++ MicrosTestTask.DAL/Configurations/OperationConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MicrosTestTask.DAL.Entities;
+
+namespace MicrosTestTask.DAL.Configurations;
+
+public class OperationConfiguration : IEntityTypeConfiguration<Operation>
+{
+    public void Configure(EntityTypeBuilder<Operation> builder)
+    {
+        builder.ToTable("Operations").HasKey(x => x.Id);
+
+        builder.Property(x => x.Date).IsRequired();
+        builder.Property(x => x.Sum).IsRequired();
+        builder.Property(x => x.CategoryId).IsRequired();
+        builder.Property(x => x.UserId).IsRequired();
+
+        builder.HasOne(x => x.Category)
+                .WithMany(x => x.Operations);
+    }
+}
