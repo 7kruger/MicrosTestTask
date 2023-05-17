f944382b113d4416e980016198da85c2da68bee7
7kruger <ekruug3r@gmail.com>
2023-05-17 14:31:17 +0500

    add automapper profile

--- MicrosTestTask.BLL/Models/CategoryModel.cs
+++ MicrosTestTask.BLL/Models/CategoryModel.cs
@@ -7,5 +7,5 @@ public class CategoryModel
     public int Id { get; set; }
     public string Name { get; set; }
     public CategoryType CategoryType { get; set; }
-    public IEnumerable<OperationModel> Operations { get; set; }
+    public IEnumerable<OperationModel> Operations { get; set; } = Enumerable.Empty<OperationModel>();
 }
