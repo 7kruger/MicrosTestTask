c614daffaa9dc9f7a932e2fa12b0d30669ff5bad
7kruger <ekruug3r@gmail.com>
2023-05-17 13:38:48 +0500

    helpers moved to domain layer

---
+++ MicrosTestTask.Domain/Helpers/PasswordHashHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using SHA3.Net;
+
+namespace MicrosTestTask.Domain.Helpers;
+
+public static class PasswordHashHelper
+{
+    public static string HashPassword(string password)
+    {
+        using (var sha3256 = Sha3.Sha3256())
+        {
+            var bytes = Sha3.Sha3256().ComputeHash(Encoding.UTF8.GetBytes(password));
+            return Convert.ToHexString(bytes).ToLower();
+        }
+    }
+}
